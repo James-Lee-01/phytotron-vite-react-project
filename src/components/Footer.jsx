084c5b4038cd96c4f006078b5114d5b82a2111af
James-Lee-01 <freebike01@gmail.com>
2023-12-18 18:27:25 +0800

    feat: add Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+// Footer
+const Footer = () => {
+  return (
+    <footer className="bg-neutral-800 p-4 text-center text-white">
+      <p>&copy; 2023 EnviroGuard. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
