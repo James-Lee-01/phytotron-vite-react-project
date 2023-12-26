5cfe4e90596268507fac1a73993efa7dc262ea1a
James-Lee-01 <freebike01@gmail.com>
2023-12-26 17:57:59 +0800

    feat: add PWA function and Workbox cache and icon

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,56 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import {VitePWA} from 'vite-plugin-pwa'
+
+const manifest = {
+  registerType: "autoUpdate",
+  includeAssets: ["**/*"],
+  devOptions: {
+    enabled: true
+  },
+  workbox: {
+    globPatterns: ["**/*{js,jsx,cdd,html,png,jpg}"],
+    runtimeCaching: [
+      {
+        urlPattern: ({url}) => {
+          return url.pathname.startsWith("/")
+        },
+        handler: "CacheFirst",
+        options: {
+          cacheName: "enviroGuard-cache",
+          cacheableResponse: {
+            statuses: [0,200]
+          }
+        }
+      }
+    ]
+  },
+  manifest: {
+  "name": "EnviroGuard",
+  "short_name": "EnviroGuard",
+  "description": "EnviroGuard is a real-time weather and environmental monitoring app.",
+  "start_url": "/enviroguard.vercel.app/",
+  "scope": "/enviroguard.vercel.app/",
+  "display": "standalone",
+  "background_color": "#fff",
+  "theme_color": "#171717",
+  "Orientation": "portrait",
+  "icons": [
+    {
+      "src": "/EnviroGuard-icon.png",
+      "sizes": "272x272",
+      "type": "image/png"
+    }
+  ],
+  
+}
+
+  }
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [react()],
+  plugins: [react(), VitePWA(manifest)],
   server: {
     port: 5001, // 這是 Vite 的端口
     fs: {
