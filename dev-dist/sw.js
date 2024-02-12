91f738ce97b5d49e1904e8868c4a994bda188ffa
James-Lee-01 <freebike01@gmail.com>
2024-02-12 23:35:53 +0800

    feat: Add "lang"  attributes to  <html> element

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-fda11f75'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.o1s5tije4so"
+    "revision": "0.hm0i84pkh38"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
